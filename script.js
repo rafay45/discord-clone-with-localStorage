bb0463ad122b8a934289805ca93fcae7c99b1464
rafay45 <rafayy579@gmail.com>
2025-03-27 01:10:52 +0500

    💫💻more addition

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function signUp(){
+function signUp() {
     window.location.href = "signup.html"
 }
-function logIn(){
+function logIn() {
     window.location.href = "login.html"
 }
-function home(){
+function home() {
     window.location.href = "home.html"
+}
+let screen = document.getElementById('screen');
+let upperAI = document.getElementById('bac-AI')
+let ai = document.getElementById('AI-msg');
+function AI() {
+    upperAI.style.background = "rgb(54, 54, 54)"
+    screen.style.display = "none"
+    ai.style.display = "block"
+}
+function left() {
+    upperAI.style.background = "none"
+    screen.style.display = "block"
+    ai.style.display = "none"
 }
