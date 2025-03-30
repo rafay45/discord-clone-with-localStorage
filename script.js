4c3be9d7009fa8e8006f5371f711385e4c2ac443
rafay45 <rafayy579@gmail.com>
2025-03-31 01:03:05 +0500

    💫💻more addition

--- script.js
+++ script.js
@@ -8,15 +8,33 @@ function home() {
     window.location.href = "home.html"
 }
 let screen = document.getElementById('screen');
-let upperAI = document.getElementById('bac-AI')
+let upperAI = document.getElementById('bac-AI');
 let ai = document.getElementById('AI-msg');
+let displayName = document.getElementById('display');
+let userName = document.getElementById('userName');
+let disPara = document.getElementById('dis-para');
+let userPara = document.getElementById('user-para');
+userName.addEventListener('focus' , function(){
+    userPara.style.display = "block"
+})
+userName.addEventListener('blur' , function(){
+    userPara.style.display = "none"
+})
+displayName.addEventListener('focus', function(){
+   disPara.style.display = "block"
+})
+displayName.addEventListener('blur', function(){
+   disPara.style.display = "none"
+})
+
+
 function AI() {
-    upperAI.style.background = "rgb(54, 54, 54)"
+    upperAI.style.backgroundColor = "rgb(54, 54, 54)"
     screen.style.display = "none"
     ai.style.display = "block"
 }
 function left() {
-    upperAI.style.background = "none"
+    upperAI.style.backgroundColor = ""
     screen.style.display = "block"
     ai.style.display = "none"
 }
