1e87fcb3c30db6f391b59f63d894832dfe27a44d
rafay45 <rafayy579@gmail.com>
2025-04-08 10:20:17 +0500

    💫💻more addition

--- script.js
+++ script.js
@@ -94,6 +94,18 @@ if (loginForm) {
         }
     });
 }
+let exit = document.getElementById('exit');
+let logout = document.getElementById('out');
+if(exit){
+    exit.addEventListener('click', function(){
+        logout.style.display = "block"
+    })
+    document.addEventListener('click', function (e) {
+        if (!exit.contains(e.target) && !logout.contains(e.target)) {
+            logout.style.display = "none";
+        }
+    });
+}
 
 function signUp() {
     window.location.href = "signup.html"
@@ -101,8 +113,23 @@ function signUp() {
 function logIn() {
     window.location.href = "login.html"
 }
+function loged() {
+    window.location.href = "login.html"
+}
 
+let screen = document.getElementById('screen');
+let upperAI = document.getElementById('bac-AI');
+let ai = document.getElementById('AI-msg');
 
-
-
-
+if(screen , upperAI , ai){
+    function AI() {
+        upperAI.style.backgroundColor = "rgb(54, 54, 54)"
+        screen.style.display = "none"
+        ai.style.display = "block"
+    }
+    function left() {
+        upperAI.style.backgroundColor = ""
+        screen.style.display = "block"
+        ai.style.display = "none"
+    }
+}
