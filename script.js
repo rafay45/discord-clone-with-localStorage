b68e745517bf1bcb527d1b340769cb9f2c248049
rafay45 <rafayy579@gmail.com>
2025-04-26 23:13:58 +0500

    Completed💻🎉

--- script.js
+++ script.js
@@ -42,26 +42,26 @@ if (userIcon) {
 if (signupForm) {
     signupForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        if(displayName.value[0] === displayName.value[0].toUpperCase()){
+        if (displayName.value[0] === displayName.value[0].toUpperCase()) {
             displayName.style.border = ""
-         if(userName.value.includes('786') || userName.value.includes('98001') || userName.value.includes('7865234')){
-            userName.style.border = ""
-            singupInfo.push(
-                singEmail.value,
-                displayName.value,
-                userName.value,
-                signpass.value,
-                signMonth.value,
-                signDate.value,
-                signYear.value
-            );
-            localStorage.setItem('saveSignupInfo', JSON.stringify(singupInfo));
-            window.location.href = "home.html";
-         }else{
-            userName.style.border = "1px solid rgb(245, 69, 69)"
-            userPara.innerHTML = `<i><b>Available</b>: ${userName.value}786 , ${userName.value}98001 , ${userName.value}7865234</i>`
-         }
-        }else{
+            if (userName.value.includes('786') || userName.value.includes('98001') || userName.value.includes('7865234')) {
+                userName.style.border = ""
+                singupInfo.push(
+                    singEmail.value,
+                    displayName.value,
+                    userName.value,
+                    signpass.value,
+                    signMonth.value,
+                    signDate.value,
+                    signYear.value
+                );
+                localStorage.setItem('saveSignupInfo', JSON.stringify(singupInfo));
+                window.location.href = "home.html";
+            } else {
+                userName.style.border = "1px solid rgb(245, 69, 69)"
+                userPara.innerHTML = `<i><b>Available</b>: ${userName.value}786 , ${userName.value}98001 , ${userName.value}7865234</i>`
+            }
+        } else {
             displayName.style.border = "1px solid rgb(245, 69, 69)"
         }
     });
@@ -147,11 +147,12 @@ if (screen, upperAI, ai) {
 let msges = document.getElementById('msges');
 let chatMsg = document.getElementById('chatMsg');
 let chatPara = document.getElementById('chatPara');
+let typing = document.getElementById('typing')
 
 let msgWords = ["Hey", "How are you?", "I'm pretty good", "What is HTML?", "What is CSS?", "What is javaScript?", "Okay! Thank you"]
-let msgAns = [`Hey`, `I'm doing great, thanks for asking! How about you?`, `Nicee! Glad to hear that — pretty good is always a win 😎` , `HTML (Hypertext Markup Language) is a standard markup language used to define <br> the structure of web pages and web applications. It uses tags and elements to <br> structure content like text, images, links, and forms, which are displayed in a <br> web browser. <br> 
+let msgAns = [`Hey`, `I'm doing great, thanks for asking! How about you?`, `Nicee! Glad to hear that — pretty good is always a win 😎`, `HTML (Hypertext Markup Language) is a standard markup language used to define <br> the structure of web pages and web applications. It uses tags and elements to <br> structure content like text, images, links, and forms, which are displayed in a <br> web browser. <br> 
 
-In simple terms, HTML creates the skeleton of a webpage, including text, headings, <br> images, links, and buttons, to present content to users in a browser.` , `CSS (Cascading Style Sheets) is a stylesheet language used to describe the presentation <br> and design of a web page written in HTML. It controls how the elements on a web page <br> should look — including their colors, fonts, spacing, layout, and responsiveness.` , `JavaScript is a high-level programming language used to create interactive, dynamic, <br> and functional web pages. It allows developers to add features like form validation, <br> animations, events, and real-time updates to a website. <br>
+In simple terms, HTML creates the skeleton of a webpage, including text, headings, <br> images, links, and buttons, to present content to users in a browser.` , `CSS (Cascading Style Sheets) is a stylesheet language used to describe the presentation <br> and design of a web page written in HTML. It controls how the elements on a web page <br> should look — including their colors, fonts, spacing, layout, and responsiveness.`, `JavaScript is a high-level programming language used to create interactive, dynamic, <br> and functional web pages. It allows developers to add features like form validation, <br> animations, events, and real-time updates to a website. <br>
 <b>In simple words</b>: JavaScript brings life and interactivity to static web pages.` , `You're very welcome! 😊 <br>
 If you ever need help with HTML, CSS, JavaScript — or anything else — just hit me up anytime. <br>
 Happy coding, ${singupInfo[1]}!
@@ -165,65 +166,79 @@ if (chatMsg) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[0]} ${singupInfo[1]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
+                        typing.style.display = "none";
                     }, 2000);
                 }
                 else if (chatMsg.value == msgWords[1]) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[1]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
-                    }, 2000);
+                        typing.style.display = "none";
+                    }, 3000);
 
                 }
                 else if (chatMsg.value == msgWords[2]) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[2]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
-                    }, 2000);
-                } 
+                        typing.style.display = "none";
+                    }, 3000);
+                }
                 else if (chatMsg.value == msgWords[3]) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[3]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
-                    }, 2000);
+                        typing.style.display = "none";
+                    }, 4000);
                 }
                 else if (chatMsg.value == msgWords[4]) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[4]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
-                    }, 2000);
+                        typing.style.display = "none";
+                    }, 4000);
                 }
                 else if (chatMsg.value == msgWords[5]) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[5]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
-                    }, 2000);
+                        typing.style.display = "none";
+                    }, 4000);
                 }
                 else if (chatMsg.value == msgWords[6]) {
                     msges.innerHTML += `<div id="chatPara"><div class="aiBox"><span id="userIcon">${singupInfo[1][0]}</span></div><span>${chatMsg.value}</span></div>`
                     msges.scrollTop = msges.scrollHeight;
                     chatMsg.value = ""
+                    typing.style.display = "flex";
                     setTimeout(() => {
                         msges.innerHTML += `<div id="chatPara"><div class="box-1"><img src="assets/ai-image.jpg" alt=""></div><span>${msgAns[6]}</span></div>`
                         msges.scrollTop = msges.scrollHeight;
-                    }, 2000);
+                        typing.style.display = "none";
+                    }, 4000);
                 }
             }
         }
